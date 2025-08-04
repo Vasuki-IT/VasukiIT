1ad779a53f13088a391d4660df2fd2494b428e91
ishanism <6596823+ishanism@users.noreply.github.com>
2025-08-04 20:15:23 +0200

    feat: update homepage message with construction notice and enhanced excitement

--- vasuki-it-homepage/src/app/page.tsx
+++ vasuki-it-homepage/src/app/page.tsx
@@ -14,14 +14,14 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Under 
+            <h1>🐍 </h1>Under 
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               construction.....
             </code>
             .
           </li>
           <li className="tracking-[-.01em]">
-            Amazing things are coming soon, stay tuned!
+            Amazing things are coming soon, stay tuned!!!!!
           </li>
         </ol>
 
