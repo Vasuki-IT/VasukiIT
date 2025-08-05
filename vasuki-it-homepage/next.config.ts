bdb3920d87dba18fb6100a4e09b8623846444c45
ishanism <6596823+ishanism@users.noreply.github.com>
2025-08-05 20:51:33 +0200

    feat: add assetPrefix to next.config.ts for correct static asset serving

--- vasuki-it-homepage/next.config.ts
+++ vasuki-it-homepage/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/VasukiIT",
+  assetPrefix: "/VasukiIT/", // enables svg and other static assets to be served correctly
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
